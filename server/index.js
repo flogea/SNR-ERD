b78738ef16de4b8631c29272befd7566df181efd
flogea <katua.logvinova@gmail.com>
2023-11-12 06:29:42 +0300

    single get requests for tmp & hum

--- server/index.js
+++ server/index.js
@@ -2,17 +2,15 @@ const express = require('express');
 const fs = require('fs');
 const xml2js = require('xml2js');
 const snmp = require('net-snmp');
+var cors = require('cors');
 
 const app = express();
 const port = 5000;
-
-let XMLArray = [];
-const resData = {};
-
-app.get('/getData', (req, res) => {
-  res.send(resData);
-});
-
+const controllerAddress = '87.251.176.36';
+const oids = {
+  oid_temp: '1.3.6.1.4.1.40418.2.2.4.1.0',
+  oid_hum: '1.3.6.1.4.1.40418.2.2.4.4',
+};
 const options = {
   port: 62161,
   retries: 1,
@@ -22,15 +20,61 @@ const options = {
   idBitsSize: 16,
 };
 
-const controllerAddress = '87.251.176.36';
+let XMLArray = [];
+const resData = {};
+const temperature = {};
+const humidity = {};
 
-const oids = {
-  oid_temp: '1.3.6.1.4.1.40418.2.2.4.1.0',
-  oid_hum: '1.3.6.1.4.1.40418.2.2.4.4',
-};
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/getData', (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.send([resData]);
+});
+
+app.get('/api/getData/temperature', (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  getParamsFromSNP({ oid: oids.oid_temp, name: 'temperature', data: temperature });
+  res.send(temperature);
+});
+
+app.get('/api/getData/humidity', (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  getParamsFromSNP({ oid: oids.oid_hum, name: 'humidity', data: humidity });
+  res.send(humidity);
+});
+
+app.post('/api/setData/newTemp', (req, res) => {
+  try {
+    const { oid, newTemp } = req.body;
+    console.log(oid, newTemp);
+    setParamsToSNP(oid, newTemp);
+    res.status(200).send('Данные успешно обработаны');
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Ошибка сервера');
+  }
+});
+
+// set new paramt to SNP
+function setParamsToSNP(oid, newTemp) {
+  const session = snmp.createSession(controllerAddress, 'public');
+
+  const type = snmp.ObjectType.Integer;
+
+  session.set({ oid, value: newTemp }, (error, varbinds) => {
+    if (error) {
+      console.error('Ошибка отправки POST запроса:', error);
+    } else {
+      console.log('POST запрос успешно отправлен:', varbinds);
+    }
+    session.close();
+  });
+}
 
 // get data from SNP
-const getParamsFromSNP = ({ oid, name }) => {
+function getParamsFromSNP({ oid, name, data }) {
   const session = snmp.createSession(controllerAddress, 'public', options);
 
   session.get([oid], (error, varbinds) => {
@@ -41,16 +85,16 @@ const getParamsFromSNP = ({ oid, name }) => {
         if (snmp.isVarbindError(varbinds[i])) {
           console.error(snmp.varbindError(varbinds[i]));
         } else {
-          console.log(varbinds);
-          const reseivedData = varbinds[0].value.toString();
+          // console.log(varbinds);
+          const reseivedData = varbinds[0]?.value?.toString();
 
-          resData[name] = { oid: oid, value: reseivedData };
+          data[name] = { name: name, oid: oid, value: reseivedData };
         }
       }
     }
     session.close();
   });
-};
+}
 
 // Parsing XML
 const xmlString = fs.readFileSync('./1.xml', 'utf8');
@@ -66,13 +110,13 @@ xml2js.parseString(xmlString, (err, result) => {
 XMLArray.map((block, index) => {
   const oid = block['$'].oid.slice(1);
   if (oid != '0.0') {
-    const blockName = block['$'].valueType;
-    oids[`${blockName}_${index}`] = `${oid}`;
+    const blockName = block['$'].name;
+    oids[blockName] = `${oid}`;
   }
 });
 
 Object.entries(oids).map((oid, index) => {
-  getParamsFromSNP({ oid: oid[1], name: oid[0] });
+  getParamsFromSNP({ oid: oid[1], name: oid[0], data: resData });
 });
 
 app.listen(port, () => {
